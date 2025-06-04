74373744f1597b3bfc8b748f4ee82b3fd7f41f35
chiefEbube <ebubechi.anyanwu@yahoo.com>
2025-06-04 19:47:16 +0100

    feat: contact us page

--- app/custom-components/Heading.tsx
+++ app/custom-components/Heading.tsx
@@ -1,8 +1,10 @@
 export default function Heading({ heading }: { heading: string }) {
-    return (
-        <div className="flex flex-col w-fit">
-            <h2 className="text-4xl md:text-5xl font-extrabold leading-tight text-white">{heading}</h2>
-            <div className="w-full h-1 bg-yellow-500 rounded-xl mt-1"></div>
-        </div>
-    );
+	return (
+		<div className="flex flex-col w-fit mb-9">
+			<h2 className="text-4xl md:text-5xl lg:text-6xl font-extrabold leading-tight text-white">
+				{heading}
+			</h2>
+			<div className="w-full h-1 bg-yellow-500 rounded-xl mt-3" />
+		</div>
+	);
 }
