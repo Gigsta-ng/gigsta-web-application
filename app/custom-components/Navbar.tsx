d5d60c0b02f3a37cdeb26364ff313f7f2ff3764e
Gigsta-gig <gigsta.gig@gmail.com>
2025-06-04 01:01:01 +0100

    Chore add GitHub pr template and workflows (#6)
    * chore(CI): add build and lint workflow on PRs & pushes to main and replaced linter from eslint to biome
    
    * fix: fix lint errors
    
    ---------
    
    Co-authored-by: chuba <chinemelumchubanwene57@gmail.com>

--- app/custom-components/Navbar.tsx
+++ app/custom-components/Navbar.tsx
@@ -1,109 +1,117 @@
 "use client";
-import React from "react";
-import Logo from "./Logo";
+import {
+	Drawer,
+	DrawerClose,
+	DrawerContent,
+	DrawerFooter,
+	DrawerTitle,
+	DrawerTrigger,
+} from "@/components/ui/drawer";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { CButton } from "./CButton";
-import Wrapper from "./Wrapper";
+import React from "react";
+import { GrContact } from "react-icons/gr";
 import { IoSettings } from "react-icons/io5";
 import { RiMenuFoldLine } from "react-icons/ri";
-import { GrContact } from "react-icons/gr";
 import { VscTools } from "react-icons/vsc";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerFooter,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
-import useMediaQuery from "../hookes/useMediaQuery";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { CButton } from "./CButton";
+import Logo from "./Logo";
+import Wrapper from "./Wrapper";
 
 const Navbar = () => {
-  const pathName = usePathname();
-  const {screenHeight, screenWidth} = useMediaQuery();
-  const navList = [
-    {
-      label: "How it Works",
-      href: "/",
-      icon: <IoSettings size={24} />,
-    },
-    {
-      label: "Service",
-      href: "/service",
-      icon: <VscTools size={24} />,
-    },
-    {
-      label: "Contact",
-      href: "/contact-us",
-      icon: <GrContact size={25} />,
-    },
-  ];
-  return (
-    <Wrapper className="w-full">
-      <div className={`flex w-full justify-between items-center ${screenHeight && screenHeight >= 60 && screenWidth! <= 620 && "fixed left-[5%] transition-all -translate-x-[2.5%] w-[95%]"} z-50 rounded-[32px] max-md:bg-black/10 max-md:p-2 max-md:px-4 max-md:backdrop-blur-lg`}>
-        <Logo />
-        <div className="flex gap-6 items-center">
-          <ul className="flex gap-6 text-[20px] max-sm:hidden">
-            {navList.map((navItem) => (
-              <Link
-                className={`${
-                  pathName === navItem.href && "text-accent-yellow"
-                } font-semibold`}
-                key={navItem.label}
-                href={navItem.href}
-              >
-                {navItem.label}
-              </Link>
-            ))}
-          </ul>
-          <CButton
-            className="max-sm:hidden"
-            onPress={() => {}}
-            variant="primary"
-          >
-            Become a pro
-          </CButton>
-          <Drawer>
-            <DrawerTrigger className="md:hidden">
-              <RiMenuFoldLine size={32} />
-            </DrawerTrigger>
-            <DrawerContent className="md:hidden">
-              <DrawerTitle className="hidden">
-                Are you absolutely sure?
-              </DrawerTitle>
-              <section className="flex justify-between px-8 py-4 gap-4">
-                {navList.map((navItem) => (
-                  <DrawerClose key={navItem.label}>
-                    <Link
-                      className={`${
-                        pathName === navItem.href
-                          ? "text-black bg-accent-yellow/20 py-[6px] px-3 rounded-3xl"
-                          : "text-gray-400 hover:bg-gray-100 hover:text-gray-500 p-[6px] rounded-lg"
-                      } font-semibold flex flex-row-reverse items-center gap-2`}
-                      href={navItem.href}
-                    >
-                      <span
-                        className={`${
-                          pathName === navItem.href ? "inline-flex" : "hidden"
-                        } text-[14px]`}
-                      >
-                        {navItem.label}
-                      </span>
-                      <span>{navItem.icon}</span>
-                    </Link>
-                  </DrawerClose>
-                ))}
-              </section>
-              <DrawerFooter>
-                <DrawerClose></DrawerClose>
-              </DrawerFooter>
-            </DrawerContent>
-          </Drawer>
-        </div>
-      </div>
-    </Wrapper>
-  );
+	const pathName = usePathname();
+	const { screenHeight, screenWidth } = useMediaQuery();
+	const navList = [
+		{
+			label: "How it Works",
+			href: "/",
+			icon: <IoSettings size={24} />,
+		},
+		{
+			label: "Service",
+			href: "/service",
+			icon: <VscTools size={24} />,
+		},
+		{
+			label: "Contact",
+			href: "/contact-us",
+			icon: <GrContact size={25} />,
+		},
+	];
+	return (
+		<Wrapper className="w-full">
+			<div
+				className={`flex w-full justify-between items-center ${
+					screenHeight &&
+					screenHeight >= 60 &&
+					// biome-ignore lint/style/noNonNullAssertion: <explanation>
+					screenWidth! <= 620 &&
+					"fixed left-[5%] transition-all -translate-x-[2.5%] w-[95%]"
+				} z-50 rounded-[32px] max-md:bg-black/10 max-md:p-2 max-md:px-4 max-md:backdrop-blur-lg`}
+			>
+				<Logo />
+				<div className="flex gap-6 items-center">
+					<ul className="flex gap-6 text-[20px] max-sm:hidden">
+						{navList.map((navItem) => (
+							<Link
+								className={`${
+									pathName === navItem.href && "text-accent-yellow"
+								} font-semibold`}
+								key={navItem.label}
+								href={navItem.href}
+							>
+								{navItem.label}
+							</Link>
+						))}
+					</ul>
+					<CButton
+						className="max-sm:hidden"
+						onPress={() => {}}
+						variant="primary"
+					>
+						Become a pro
+					</CButton>
+					<Drawer>
+						<DrawerTrigger className="md:hidden">
+							<RiMenuFoldLine size={32} />
+						</DrawerTrigger>
+						<DrawerContent className="md:hidden">
+							<DrawerTitle className="hidden">
+								Are you absolutely sure?
+							</DrawerTitle>
+							<section className="flex justify-between px-8 py-4 gap-4">
+								{navList.map((navItem) => (
+									<DrawerClose key={navItem.label}>
+										<Link
+											className={`${
+												pathName === navItem.href
+													? "text-black bg-accent-yellow/20 py-[6px] px-3 rounded-3xl"
+													: "text-gray-400 hover:bg-gray-100 hover:text-gray-500 p-[6px] rounded-lg"
+											} font-semibold flex flex-row-reverse items-center gap-2`}
+											href={navItem.href}
+										>
+											<span
+												className={`${
+													pathName === navItem.href ? "inline-flex" : "hidden"
+												} text-[14px]`}
+											>
+												{navItem.label}
+											</span>
+											<span>{navItem.icon}</span>
+										</Link>
+									</DrawerClose>
+								))}
+							</section>
+							<DrawerFooter>
+								<DrawerClose />
+							</DrawerFooter>
+						</DrawerContent>
+					</Drawer>
+				</div>
+			</div>
+		</Wrapper>
+	);
 };
 
 export default Navbar;
---
+++ app/hooks/useMediaQuery.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+const useMediaQuery = () => {
+	const [screenWidth, setScreenWidth] = useState<number>();
+	const [screenHeight, setScreenHeight] = useState<number>();
+
+	const updateScreenWidth = () => {
+		setScreenWidth(window.innerWidth);
+		setScreenHeight(window.innerHeight);
+	};
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		updateScreenWidth();
+
+		window.addEventListener("resize", updateScreenWidth);
+
+		return () => window.removeEventListener("resize", updateScreenWidth);
+	}, [screenHeight, screenWidth]);
+
+	return { screenHeight, screenWidth };
+};
+
+export default useMediaQuery;
